92feeebe1b284e0ab536054c47336b66e2131368
ShrutiBelsare27 <shrutibelsare27@gmail.com>
2021-10-25 00:25:48 +0530

    Added: flip coin

---
+++ 1/FlipCoin/FlipCoin/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace FlipCoin
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            FlipCoin1 obj1 = new FlipCoin1();
+            obj1.flipCoin();
+        }
+    }
+
+}
+
