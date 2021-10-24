d27be2e71a86b6d2570bbfdfb9d3da69a4c3773f
ShrutiBelsare27 <shrutibelsare27@gmail.com>
2021-10-24 23:56:02 +0530

    Added: Swap number

---
+++ 7/SwapTwoNumber/SwapTwoNumber/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SwapTwoNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 26, b = 25;
+            Console.WriteLine("Before swap a= " + a + " b= " + b);
+            a = a + b;
+            b = a - b;
+            a = a - b;
+            Console.Write("After swap a= " + a + " b= " + b);
+        }
+    }
+}
